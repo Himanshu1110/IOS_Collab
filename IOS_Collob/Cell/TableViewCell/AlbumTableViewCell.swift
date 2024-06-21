64014271ce6d2086f1edbdce6938cb2ea01f8807
utsav3108 <pandyautsav3108@gmail.com>
2024-06-21 19:10:16 +0530

    (Utsav) fixed issues in Home Screen and added Side Bar.

--- IOS_Collob/Cell/TableViewCell/AlbumTableViewCell.swift
+++ IOS_Collob/Cell/TableViewCell/AlbumTableViewCell.swift
@@ -9,26 +9,30 @@ import UIKit
 
 protocol MovetoScreen{
     func MoveToDetailScreen(ImageName : String)
+    func MovetoSingerProfileScreen(index: Int)
 }
 
 class AlbumTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    @IBOutlet weak var NewReleaseCollectionView: UICollectionView!
+    // MARK: - Ib Outlet Collection View
+    @IBOutlet weak var AlbumCollectionView: UICollectionView!
     
+    // MARK: - Static Data
     var albums = [""]
     var albumNames = [""]
     var albumSingers = [""]
+    var isSingers = false
     
+    
+    var delegateSinger: MovetoScreen!
     var delegate : MovetoScreen!
     
+    // MARK: - Table View Cell Delegates
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        NewReleaseCollectionView.delegate = self
-        NewReleaseCollectionView.dataSource = self
-        
-//        var delegate : MovetoScreen!
-    
+        AlbumCollectionView.delegate = self
+        AlbumCollectionView.dataSource = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -37,6 +41,7 @@ class AlbumTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
         
     }
     
+    // MARK: - Collection Delegates
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         albums.count
     }
@@ -44,19 +49,29 @@ class AlbumTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // Providing the Data Source
-        let cell = NewReleaseCollectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCollectionViewCell", for: indexPath) as! AlbumCollectionViewCell
+        let cell = AlbumCollectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCollectionViewCell", for: indexPath) as! AlbumCollectionViewCell
         
         
         cell.imageAlbum.image = UIImage(named: albums[indexPath.item])
-        cell.imageAlbum.applyCornerRadius(radius: 20)
-        cell.lbAlbumName.text = albumNames[indexPath.item]
         cell.lbSingerName.text = albumSingers[indexPath.item]
+        
+        if isSingers {
+            cell.imageAlbum.makeCircle()
+            cell.lbAlbumName.isHidden = true
+            cell.lbSingerName.font = .systemFont(ofSize: 15)
+            cell.lbSingerName.translatesAutoresizingMaskIntoConstraints = false
+            cell.lbSingerName.textAlignment = .center
+            cell.lbSingerName.topAnchor.constraint(equalTo: cell.imageAlbum.bottomAnchor, constant: 10).isActive = true
+        }else{
+            cell.imageAlbum.applyCornerRadius(radius: 20)
+            cell.lbAlbumName.text = albumNames[indexPath.item]
+        }
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 104, height: 153)
+        return CGSize(width: 134, height: 153)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -65,6 +80,9 @@ class AlbumTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectio
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        if isSingers {
+            delegateSinger.MovetoSingerProfileScreen(index: indexPath.item)
+        }
         delegate?.MoveToDetailScreen(ImageName: albums[indexPath.item] )
 
     }
--- IOS_Collob/Extension/Extensions.swift
+++ IOS_Collob/Extension/Extensions.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+
+// MARK: - Home VC User Defined Functions
 extension HomeViewController {
     func setGradientBackground(view: UIView) {
         let colorTop = UIColor(named: "TopColour")!.cgColor
@@ -19,15 +21,163 @@ extension HomeViewController {
                 
         view.layer.insertSublayer(gradientLayer, at:0)
     }
+
+    func setupUI(){
+        //Customize the Search Text Field
+        tfSearch.borderStyle = .none
+        tfSearch.attributedPlaceholder =
+        NSAttributedString(string: "Find your Favourite Music!", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+
+        // Customize the Search View
+        viewSearch.applyCornerRadius(radius: 20)
+        
+        viewSearch.backgroundColor = .systemGray6
+        
+        viewSearch.dropShadow()
+    }
 }
 
+// MARK: - Move To Different Screens
+extension HomeViewController: MovetoScreen {
+    
+    func MoveToDetailScreen(ImageName: String) {
+        let DetailsScreenObj = UIStoryboard(name: "DetailScreen", bundle: nibBundle).instantiateViewController(withIdentifier: "DetailsScreenVC") as! DetailsScreenVC
+        
+        DetailsScreenObj.imageCover = ImageName
+       DetailsScreenObj.hidesBottomBarWhenPushed = true
+        
+        self.navigationController?.pushViewController(DetailsScreenObj, animated: true)
+    }
+    
+    func MovetoSingerProfileScreen(index: Int){
+        let profile = UIStoryboard(name: "UserProfile", bundle: nibBundle).instantiateViewController(withIdentifier: "UserProfileVC") as! UserProfileVC
+        //profile.index = index
+        self.navigationController?.pushViewController(profile, animated: true)
+    }
+    
+    @objc func MoveToPreviousScreen(){
+        print("Move To Previous Screen")
+        self.tabBarController?.navigationController?.popViewController(animated: true)
+        //self.navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - HomeVC Table View Delegates
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        /*
+         1. New Release
+         2. Popular
+         3. Top Singer
+         */
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        // Develop Custom Header Here
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 40))
+        
+        // Take a Label for Section Title
+        let label = UILabel()
+        label.frame = CGRect.init(x: 30, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
+        label.text = sectionTitles[section]
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .black
+
+        // Add the Label in headerView
+        headerView.addSubview(label)
+        
+        return headerView
+        
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        // Provide the height to each section
+        return 40
+    }
+        
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // No. of row each section contains
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // Save the section index in index variable.
+        let index = indexPath.section
+        
+        // Every index will have their own Contents, but the Prototype Cell will be the same for each section.
+        if index == 0{
+            let cell = HomeTableView.dequeueReusableCell(withIdentifier: "AlbumTableViewCell", for: indexPath) as! AlbumTableViewCell
+            cell.delegate = self
+            cell.albums = newReleasesAlbum
+            cell.albumNames = newReleaseAlbumName
+            cell.albumSingers = newReleaseAlbumSingers
+            return cell
+        }else if index == 1{
+            let cell = HomeTableView.dequeueReusableCell(withIdentifier: "AlbumTableViewCell", for: indexPath) as! AlbumTableViewCell
+            cell.delegate = self
+            cell.albums = popularAlbum
+            cell.albumNames = popularAlbumName
+            cell.albumSingers = popularAlbumSingers
+            return cell
+        }else{
+            
+            // Top Singers section have different fields hence to know that it is Top Singer section isSingers variable will be set to true.
+            let cell = HomeTableView.dequeueReusableCell(withIdentifier: "AlbumTableViewCell", for: indexPath) as! AlbumTableViewCell
+            cell.isSingers = true
+            cell.delegateSinger = self
+            cell.albums = topSingers
+            cell.albumSingers = topSingerNames
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        // Height for the Cells
+        let index = indexPath.section
+        
+        if index == 2{
+            // Top Singer Section
+            return 145
+        }else{
+            
+            // New Release and Popular Songs Section
+            return 160
+        }
+        
+    }
+}
+
+// MARK: - UIView Extension
 extension UIView {
     func applyCornerRadius(radius: CGFloat) {
+        
+        // Set CornerRadius provided by the user and clips to bounds true
         self.layer.cornerRadius = radius
         self.clipsToBounds = true
     }
     
     func makeCircle(){
+        
+        // Half the width of frame of given view and call applyCornerRadius()
         self.applyCornerRadius(radius: self.frame.width / 2)
+    }
+    
+    func dropShadow() {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOffset = CGSize(width: -1, height: 2)
+        self.layer.shadowRadius = 1
+
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
     }
 }
